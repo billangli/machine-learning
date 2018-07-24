392d22ab2bef5d85dfc82330a1999a0958c12634
Ang Li <billbillbill.li@mail.utoronto.ca>
2018-07-24 17:16:44 -0400

    Complete backprop for network.py

--- src/network.py
+++ src/network.py
@@ -73,56 +73,60 @@ class Network(object):
         self.weights = [w - eta / len(mini_batch) * nw for w, nw in zip(self.weights, nabla_w)]
 
     def backprop(self, x, y):
-        """Return a tuple ``(nabla_b, nabla_w)`` representing the
-        gradient for the cost function C_x.  ``nabla_b`` and
-        ``nabla_w`` are layer-by-layer lists of numpy arrays, similar
-        to ``self.biases`` and ``self.weights``."""
+        """
+        Return a tuple (nabla_b, nabla_w) representing the gradient for the cost function C_x
+        Each of <nabla_b> and <nabla_w> are lists of NumPy arrays
+            similar in structure to <self.biases> and <self.weights>
+        """
         nabla_b = [np.zeros(b.shape) for b in self.biases]
         nabla_w = [np.zeros(w.shape) for w in self.weights]
-        # feedforward
+
+        # Feed forward
         activation = x
-        activations = [x] # list to store all the activations, layer by layer
-        zs = [] # list to store all the z vectors, layer by layer
+        activations = [x]
+        zs = []  # Store the weighted inputs of each layer (z before activation function)
         for b, w in zip(self.biases, self.weights):
-            z = np.dot(w, activation)+b
+            z = np.dot(w, activation) + b
             zs.append(z)
             activation = sigmoid(z)
             activations.append(activation)
-        # backward pass
-        delta = self.cost_derivative(activations[-1], y) * \
-            sigmoid_prime(zs[-1])
+
+        # Backward pass
+        delta = self.cost_derivative(activations[-1], y) * sigmoid_prime(zs[-1])
         nabla_b[-1] = delta
         nabla_w[-1] = np.dot(delta, activations[-2].transpose())
-        # Note that the variable l in the loop below is used a little
-        # differently to the notation in Chapter 2 of the book.  Here,
-        # l = 1 means the last layer of neurons, l = 2 is the
-        # second-last layer, and so on.  It's a renumbering of the
-        # scheme in the book, used here to take advantage of the fact
-        # that Python can use negative indices in lists.
+
+        # Back propagate (l index starts with last layer being 1)
         for l in range(2, self.num_layers):
             z = zs[-l]
-            sp = sigmoid_prime(z)
-            delta = np.dot(self.weights[-l+1].transpose(), delta) * sp
+            delta = np.dot(self.weights[-l + 1].transpose(), delta) * sigmoid_prime(z)
             nabla_b[-l] = delta
             nabla_w[-l] = np.dot(delta, activations[-l-1].transpose())
+
         return (nabla_b, nabla_w)
 
     def evaluate(self, test_data):
-        """Return the number of test inputs for which the neural
-        network outputs the correct result. Note that the neural
-        network's output is assumed to be the index of whichever
-        neuron in the final layer has the highest activation."""
-        test_results = [(np.argmax(self.feed_forward(x)), y)
-                        for (x, y) in test_data]
+        """
+        Return the number of test inputs for which the neural network outputs the correct result.
+        Note that the neural network's output is assumed to be the index of whichever neuron in the final layer has the
+            highest activation.
+        """
+        test_results = [(np.argmax(self.feed_forward(x)), y) for (x, y) in test_data]
         return sum(int(x == y) for (x, y) in test_results)
 
     def cost_derivative(self, output_activations, y):
-        """Return the vector of partial derivatives \partial C_x /
-        \partial a for the output activations."""
+        """
+        Return the vector of partial derivatives \partial C_x / \partial a for the output activations
+
+        Based on the quadratic cost function C(x) = 1/(2n) * (y - a^L(x))^2
+            where x is input
+            y is expected output
+            a^L is the actual output
+        """
         return (output_activations-y)
 
 
-def sigmoid(z) -> float:
+def sigmoid(z):
     """Sigmoid function"""
     return 1.0 / (1.0 + np.exp(-z))
 
